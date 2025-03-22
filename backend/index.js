fbb0e50561363da304d036ec48048011d8b8d78e
Proxima prime <Proxima prime@DESKTOP-4OAR5T1>
2025-03-22 15:14:35 +0700

    Final version as first course work.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import express from "express"
 import cors from "cors";
 
@@ -7,46 +7,85 @@ const PORT = 5000;
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000", // Разрешаем только этот источник
+    methods: ["GET", "POST", "PUT", "DELETE"], // Разрешаем только нужные методы
+    allowedHeaders: ["Content-Type"] // Разрешаем заголовки
+}))
+app.use(express.json())
+
 
 app.listen(PORT, () => {console.log("Server is running on port " + PORT)})
 
-//
-const subjects = JSON.parse(fs.readFileSync("./backend/subjects.json", 'utf8'));
-console.log(subjects)
-//
 
-//
-const notesLists = JSON.parse(fs.readFileSync("./backend/notesLists.json", 'utf8'));
-console.log(notesLists)
-//
+app.post("/post", async (req, res) => {
 
-// app.post("/post", (req, res) => {
-//     res.status(200).json({
-//         subjects,
-//         notesLists
-//     })
-// })
+        const data = req.body;
+        const postObj = {
+            id: data.id,
+            title: data.title,
+            subTitle: data.subTitle,
+            notes: data.notes,
+        };
 
-app.get("/get", (req, res) => {
+        console.log("Запрос отправлен");
+
+        // Читаем существующие данные
+        const subjects = JSON.parse(await fs.readFile("./backend/subjects.json", 'utf8'));
+        subjects.push(postObj);
+        //
+        // // Записываем новые данные
+        await fs.writeFile("./backend/subjects.json", JSON.stringify(subjects, null, 2));
+
+        res.status(200).json({ message: "Запись добавлена" });
+
+});
+
+app.get("/get", async (req, res) => {
+    const subjects = JSON.parse(await fs.readFile("./backend/subjects.json", 'utf8'));
+
     res.status(200).json({
-        subjects,
-        notesLists
+        subjects
     })
 })
 
-// app.put("/put", (req, res) => {
-//     res.status(200).json({
-//         subjects,
-//         notesLists
-//     })
-// })
-//
-// app.delete("/delete", (req, res) => {
-//     res.status(200).json({
-//         subjects,
-//         notesLists
-//     })
-// })
+app.put("/update", async (req, res) => {
+    const {SSI, note, add} = req.body;
+
+    console.log("Запрос отправлен");
+
+    // Читаем существующие данные
+    const subjects = JSON.parse(await fs.readFile("./backend/subjects.json", 'utf8'));
+    let updatedSubjects = [...subjects];
+    if(add){
+       updatedSubjects[SSI].notes.push(note);
+    } else {
+        updatedSubjects[SSI] = {
+            ...updatedSubjects[SSI],
+            notes: subjects[SSI].notes.filter(n => n.id !== note.id)
+        }
+    }
+
+    // Записываем новые данные
+    await fs.writeFile("./backend/subjects.json", JSON.stringify(updatedSubjects, null, 2));
+
+    res.status(200).json({ message: "Запись добавлена" });
+})
+
+app.delete("/delete", async (req, res) => {
+    console.log("ай")
+    const {SSI, subject} = req.body;
+    console.log(SSI + " " + subject);
+
+    // Читаем существующие данные
+    const subjects = JSON.parse(await fs.readFile("./backend/subjects.json", 'utf8'));
+    subjects.splice(SSI, 1);
+    //
+    // // Записываем новые данные
+    await fs.writeFile("./backend/subjects.json", JSON.stringify(subjects, null, 2));
+
+    res.status(200).json({ message: "Запись добавлена" });
+})
+
 
 
